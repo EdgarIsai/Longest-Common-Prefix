c2c5f31868c5b9848a3e680c82b8d6bc649ae89d
Edgar Isai <edgarisaiwr@gmail.com>
2019-10-29 22:27:00 -0600

    fully working with any char

--- prefijos.py
+++ prefijos.py
@@ -1,6 +1,7 @@
-lista = ['alec', 'ejemplo', 'antony', 'alexa', 'foca']
+lista = ['alec', 's', 'alarma', 'asetat', 'alexis', 'alec']
 prefijo = []
 prefijos_finales = []
+
 for palabra in lista:
     prefijo_valido = 1
     if not prefijo:
@@ -9,6 +10,7 @@ for palabra in lista:
 
         # Compara con las demas palabras para ver si cumple el prefijo
         n_excepciones = 0
+        # Busca el prefijo comparando con las otras palabras
         for candidato in lista:
             # Si el candidato es palabra y no ha aparecido
             if candidato == palabra and n_excepciones == 0:
@@ -31,13 +33,15 @@ for palabra in lista:
     if prefijo:
         # Aumentamos el prefijo en una palabra
         posible = 1
+        # Mientras pueda existir un prefijo mas grande:
         while posible:
             n_excepciones = 0
-            # En caso de que el prefijo sea de la misma longitud que 
+            # En caso de que el prefijo sea de la misma longitud que
             # la palabra se sale
             if len(prefijo[0]) == len(palabra) and prefijo_valido == 0:
                 posible = 0
                 continue
+            # Busca el prefijo comparando con las otras palabras
             for candidato in lista:
                 bandera = candidato[:len(prefijo[0])]
                 # Si el candidato es palabra y no ha aparecido
@@ -45,29 +49,41 @@ for palabra in lista:
                     n_excepciones += 1
                     continue
                 # Si se encuentra el prefijo
-                
+
                 elif prefijo[0] == candidato[:len(prefijo[0])]:
                     prefijo_valido = 1
                     posible = 1
                     break
                 # No se encontro prefijo usando palabra como candidato
+                elif len(candidato) < len(prefijo[0]):
+                    prefijo_valido = 0
+                    posible = 0
+                    continue
+
                 else:
                     prefijo_valido = 0
                     posible = 0
 
-                if len(prefijo[0]) == len(palabra):
+                if prefijo[0] == candidato:
+                    prefijo_valido = 1
+                    posible = 0
+                    continue
+
+                if prefijo[0] == candidato:
                     break
             # Si el prefijo que se intento no es valido se regresa al prefijo
             # anterior
             if not prefijo_valido:
                 prefijo[0] = palabra[:len(prefijo[0])-1]
-            # Agrega una letra al prefijo en caso de que el prefijo intentado sea valido
-            else:
+            # Agrega una letra al prefijo en caso de que el prefijo
+            # intentado sea valido
+            elif len(prefijo[0])+1 <= len(palabra):
                 prefijo[0] = palabra[:len(prefijo[0])+1]
+            else:
+                break
         # Agregamos el posible prefijo
         prefijos_finales.append(prefijo[0])
         prefijo = []
-    lista.remove(palabra)
 
 # Limpia los prefijos finales para dejar prefijos validos
 if len(prefijos_finales) > 0:
